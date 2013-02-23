13e23122c44263bfd4ddf02b188ef87d6e978d2f
Lingliang Zhang <lz781@nyu.edu>
2013-02-23 12:31:36 +0400

    validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,19 @@
 class Event < ActiveRecord::Base
-  attr_accessible :description, :end_time, :host_id, :image, :location, :name, :start_time, :status
+  attr_accessible :description, :end_time, :user_id, :image, :location, :name, :start_time, :status
 
   belongs_to :user
+
+  validate :start_time_cannot_be_in_the_past, :end_time_cannot_be_before_start_time
+
+  def start_time_cannot_be_in_the_past
+    if !start_time.blank? and start_time  < DateTime.now
+      errors.add(:start_time, "cannot be in the past")
+    end
+  end
+
+  def end_time_cannot_be_before_start_time
+    if !end_time.blank? and end_time < start_time
+      errors.add(:end_time, "cannot be before start time")
+    end
+  end
 end
