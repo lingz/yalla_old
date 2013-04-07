ee2050fedf7863731efd12dceddfd8aef65282c6
lingz <lz781@nyu.edu>
2013-04-07 22:14:34 +0400

    fixing migration1

--- db/migrate/20130402104830_remove_user_event_id_from_users.rb
+++
@@ -1,9 +0,0 @@
-class RemoveUserEventIdFromUsers < ActiveRecord::Migration
-  def up
-    remove_column :users, :user_event_id
-  end
-
-  def down
-    add_column :users, :user_event_id, :integer
-  end
-end
