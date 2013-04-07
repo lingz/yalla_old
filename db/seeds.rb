6a3836a998f2d9462384b17180e6e4a398446391
lingz <lz781@nyu.edu>
2013-04-08 01:58:21 +0400

    fixing localhost13

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-calendar = Calendar.create({code: "4/sKj-4D97VtQ7nHKIryrRa-yEPnzu.0trI63WI3O8XaDn_6y0ZQNgGcoIOegI",
+calendar = Calendar.create({code: "4/4Mt_ULTtrxwsNq_osEgXcepzwe9D.4mHA91ZWNwQYaDn_6y0ZQNiS4vTMewI",
                            client_id: "759068570332.apps.googleusercontent.com",
                            client_secret: "7amw71XAAyReH92K_LtOp5-a",
                            redirect_uri: "https://localhost/oauth2callback",
-                           access_token: "ya29.AHES6ZQsX4fsVjNY9G3a9TOU3-h49X3r128DaWNPv_YCMuK3c8rXXQ",
-                           refresh_token: "1/6pC6Dwsax6BzqZOfJ3C28e7K9wBUEv5uYmOvfRNQ560",
-                           calendar_id: "almultaqa.events@gmail.com",
+                           access_token: "ya29.AHES6ZQQk4viAeJNsnx9HKbigvCttSGrgDbxOSfyI4XDFEpkRLcddQkkk",
+                           refresh_token: "1/m6hI1KK-dLsFyD6jnLaTaNao9QMk-86pLE_0-6h_LdU",
+                           calendar_id: "nyuad.yalla@gmail.com",
                            last_update: DateTime.now.advance(days: -7)})
 calendar.save!
 
