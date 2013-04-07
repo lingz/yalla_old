bdd9621ba17e10a88e82bd1ecc7c72e114ca2259
lingz <lz781@nyu.edu>
2013-04-07 23:59:59 +0400

    fixing localhost9

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-config.assets.precompile += %w( jquery.js )
-config.assets.precompile += %w( jquery.masonry.min.js )
-config.assets.precompile += %w( jquery-ui-1.10.2.custom.js )
-config.assets.precompile += %w( base.js )
-config.assets.precompile += %w( bootstrap.js )
-
 # Initialize the rails application
 Unilink::Application.initialize!
